17416c87daaaf7f85de4b41d7503bea4ec8c83a3
yujian.jiang <yujian.jiang@zatech.com>
2024-08-13 00:20:54 +0800

    feat: add

---
+++ medium/chainable-option/template.ts
@@ -0,0 +1,11 @@
+// type Chainable<T = {}> = {
+//     option: <K extends string, V>(key: K extends keyof T ?
+//       V extends T[K] ? never : K
+//       : K, value: V) => Chainable<Omit<T, K> & Record<K, V>>
+//     get: () => T
+//   }
+
+type Chainable<T={}> ={
+    option:<K extends string,V>(key:K ,value:V)=>Chainable< Omit<T,K> & Record<K,V>>
+    get:()=>T
+}
